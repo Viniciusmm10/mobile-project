4027d6f4f3268b7bd361cc4bd91cc78145d460ca
Viniciusmm10 <93295720+Viniciusmm10@users.noreply.github.com>
2024-08-06 14:32:00 -0300

    init 06/08 14:31

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,26 +1,12 @@
 extends Node3D
 
-@export var sensitivity := 0.2
-@export var acceleration := 10
+@onready var player: NodePath
 
-const MIN := -300
-const MAX := 250
+@onready var player_node: Node3D = get_node(player)
 
-var cam_hor := 0.0
-var cam_ver := 0.0
+var camera_offset: Vector3 = Vector3(0, 10, -10)
 
-func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		cam_hor -= event.relative.x * sensitivity
-		cam_ver -= event.relative.y * sensitivity
-		
-	if Input.is_action_just_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
-func _physics_process(delta: float) -> void:
-	cam_ver = clamp(cam_ver, MIN, MAX)
-	$horizontal.rotation_degrees.y = lerp($horizontal.rotation_degrees.y, cam_hor, acceleration * delta)
-	$horizontal/vertical.rotation_degrees.x = lerp($horizontal.rotation_degrees.x, cam_ver, acceleration * delta)
+func _process(delta: float) -> void:
+	if player_node:
+		global_transform.origin = player_node.global_transform.origin + camera_offset
+		look_at(player_node.global_transform.origin, Vector3.UP)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -23,9 +23,8 @@ func _physics_process(delta: float) -> void:
 			_attack()
 			print_debug("atacar inimigo")
 
-	var horizontal_rotation = $camera/horizontal.global_transform.basis.get_euler().y
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized().rotated(Vector3.UP, horizontal_rotation)
+	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
 	if velocity.length() > 0:
 		animation_player.play("run")
 	else:
